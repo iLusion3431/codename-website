b632f6c7fbb2727e422b66e4cc95fc12ea01d833
Ne_Eo <NeeEoo@users.noreply.github.com>
2024-10-12 21:08:55 +0200

    Added support for desc

---
+++ featured-mods/gorefield/desc.md
@@ -0,0 +1,20 @@
+## HAPPY 2ND ANNIVERSARY FROM GOREFIELD MOD!
+
+<span style="color:#6EE16C">The lasagna is ready!</span>
+
+After 2 long years, the story of Jon and Bf vs. Gorefield continues, with better quality and content. An adventure full of terrors and enigmas awaits you, a renewed mod that quadruples the content of the first version!
+
+The mod currently contains:
+
+More than 12 songs
+- Scares
+- Difficulty
+- Secrets
+- ESP/ENG languages
+- And more!
+
+
+## <span style="color:#FF4E4E">WARNING!</span>
+The mod contains scares and many other things that may not be suitable for some players, player discretion is advised.
+
+PS: If you like the mod, thank Bitfox, he suffered and dedicated 2 years of his life; If it weren't for him, V2 would not exist.
--- src/pages/mods.build.js
+++ src/pages/mods.build.js
@@ -1,156 +1,168 @@
 var fs = require('fs');
+const { Remarkable } = require('remarkable');
 
 const { fixHtmlRefs, parseTemplate } = require("../utils.js");
 
 var modsDir = "./featured-mods/";
 
 function getValidMods() {
-    var mods = [];
+	var mods = [];
 
-    var files = fs.readdirSync(modsDir);
-    for(const file of files) {
-        if(file.startsWith(".")) continue;
-        if(!fs.existsSync(modsDir + file + "/meta.json")) continue;
+	var files = fs.readdirSync(modsDir);
+	for(const file of files) {
+		if(file.startsWith(".")) continue;
+		if(!fs.existsSync(modsDir + file + "/meta.json")) continue;
 
-        mods.push(file);
-    }
+		mods.push(file);
+	}
 
-    return mods;
+	return mods;
 }
 
 function buildMods(pageDir, exportPath) {
-    var warnings = [];
+	var warnings = [];
 
-    var mods = [];
-    var links = {};
-    for(const mod of getValidMods()) {
-        var meta = JSON.parse(fs.readFileSync(modsDir + mod + "/meta.json", 'utf8'));
+	var mods = [];
+	var links = {};
+	for(const mod of getValidMods()) {
+		var meta = JSON.parse(fs.readFileSync(modsDir + mod + "/meta.json", 'utf8'));
 
-        let modExport = exportPath + "mods/" + mod;
+		let modExport = exportPath + "mods/" + mod;
 
-        if(!fs.existsSync(modExport)) fs.mkdirSync(modExport, { recursive: true });
+		if(!fs.existsSync(modExport)) fs.mkdirSync(modExport, { recursive: true });
 
-        var imageExt = null;
-        if(fs.existsSync(modsDir + mod + "/cover.jpg")) {
-            imageExt = "jpg";
-        }
-        else if(fs.existsSync(modsDir + mod + "/cover.jpeg")) {
-            imageExt = "jpeg";
-        }
-        else if(fs.existsSync(modsDir + mod + "/cover.png")) {
-            imageExt = "png";
-        }
+		var imageExt = null;
+		if(fs.existsSync(modsDir + mod + "/cover.jpg")) {
+			imageExt = "jpg";
+		}
+		else if(fs.existsSync(modsDir + mod + "/cover.jpeg")) {
+			imageExt = "jpeg";
+		}
+		else if(fs.existsSync(modsDir + mod + "/cover.png")) {
+			imageExt = "png";
+		}
 
-        var imgLink;
+		var imgLink;
 
-        if(imageExt == null) {
-            //warnings.push("No cover image found for mod: " + meta.name);
-            imgLink = "img/missing.png";
-        } else {
-            fs.copyFileSync(modsDir + mod + "/cover." + imageExt, modExport + "/cover." + imageExt);
-            imgLink = "./mods/" + mod + "/cover." + imageExt;
-        }
+		if(imageExt == null) {
+			//warnings.push("No cover image found for mod: " + meta.name);
+			imgLink = "img/missing.png";
+		} else {
+			fs.copyFileSync(modsDir + mod + "/cover." + imageExt, modExport + "/cover." + imageExt);
+			imgLink = "./mods/" + mod + "/cover." + imageExt;
+		}
 
-        if(meta.link != null && meta.link != "") {
-            if(links[meta.link]) {
-                warnings.push("Duplicate link: " + meta.link + " (mod: " + meta.name + " and " + links[meta.link] + ")");
-            }
-            links[meta.link] = meta.name;
-        }
+		if(meta.link != null && meta.link != "") {
+			if(links[meta.link]) {
+				warnings.push("Duplicate link: " + meta.link + " (mod: " + meta.name + " and " + links[meta.link] + ")");
+			}
+			links[meta.link] = meta.name;
+		}
 
-        var tags = meta.tags ?? [];
+		var tags = meta.tags ?? [];
 
-        mods.push({
-            name: meta.name,
-            description: meta.description,
-            image: imgLink,
-            link: meta.link,
-            tags: tags,
-            tagsRaw: (tags).join(","),
-            author: meta.author,
-            source: meta.source,
-            version: meta.version,
-            lastUpdated: meta.lastUpdated ?? "unknown",
-            premium: tags.includes("premium")
-        });
-    }
+		mods.push({
+			name: meta.name,
+			description: meta.description,
+			image: imgLink,
+			link: meta.link,
+			tags: tags,
+			tagsRaw: (tags).join(","),
+			author: meta.author,
+			source: meta.source,
+			version: meta.version,
+			lastUpdated: meta.lastUpdated ?? "unknown",
+			premium: tags.includes("premium")
+		});
+	}
 
-    mods.sort((a, b) => a.name.localeCompare(b.name));
-    return {mods, warnings};
+	mods.sort((a, b) => a.name.localeCompare(b.name));
+	return {mods, warnings};
 }
 
 var header = fs.readFileSync("./src/pages/templates/header.html", 'utf8');
 
 function buildHtml(_pageDir, _exportPath) {
-    var pageDir = _pageDir + "mods/";
-    var exportPath = _exportPath + "mods/";
-    if (!fs.existsSync(exportPath)) {
-        fs.mkdirSync(exportPath, {recursive: true});
-    }
+	var pageDir = _pageDir + "mods/";
+	var exportPath = _exportPath + "mods/";
+	if (!fs.existsSync(exportPath)) {
+		fs.mkdirSync(exportPath, {recursive: true});
+	}
 
-    var mods = getValidMods();
+	var mods = getValidMods();
 
-    var templatePage = fs.readFileSync("./src/pages/templates/mod.html", 'utf8');
+	var templatePage = fs.readFileSync("./src/pages/templates/mod.html", 'utf8');
 
-    for(const mod of mods) {
-        var meta = JSON.parse(fs.readFileSync(modsDir + mod + "/meta.json", 'utf8'));
+	var renderer = new Remarkable({
+		html: true,
+	});
 
-        let modExport = exportPath + mod;
+	for(const mod of mods) {
+		var meta = JSON.parse(fs.readFileSync(modsDir + mod + "/meta.json", 'utf8'));
 
-        var imageExt = null;
-        if(fs.existsSync(modsDir + mod + "/cover.jpg")) {
-            imageExt = "jpg";
-        }
-        else if(fs.existsSync(modsDir + mod + "/cover.jpeg")) {
-            imageExt = "jpeg";
-        }
-        else if(fs.existsSync(modsDir + mod + "/cover.png")) {
-            imageExt = "png";
-        }
+		let modExport = exportPath + mod;
 
-        var imgLink;
+		var imageExt = null;
+		if(fs.existsSync(modsDir + mod + "/cover.jpg")) {
+			imageExt = "jpg";
+		}
+		else if(fs.existsSync(modsDir + mod + "/cover.jpeg")) {
+			imageExt = "jpeg";
+		}
+		else if(fs.existsSync(modsDir + mod + "/cover.png")) {
+			imageExt = "png";
+		}
 
-        if(imageExt == null) {
-            //warnings.push("No cover image found for mod: " + meta.name);
-            imgLink = "img/missing.png";
-        } else {
-            fs.copyFileSync(modsDir + mod + "/cover." + imageExt, modExport + "/cover." + imageExt);
-            imgLink = "root/mods/" + mod + "/cover." + imageExt;
-        }
+		var imgLink;
 
-        var tags = meta.tags ?? [];
+		if(imageExt == null) {
+			//warnings.push("No cover image found for mod: " + meta.name);
+			imgLink = "img/missing.png";
+		} else {
+			fs.copyFileSync(modsDir + mod + "/cover." + imageExt, modExport + "/cover." + imageExt);
+			imgLink = "root/mods/" + mod + "/cover." + imageExt;
+		}
 
-        var vars = {
-            modName: meta.name,
-            header: header,
-            pageTitle: meta.name,
-            cover: imgLink,
-            description: meta.description,
-            link: meta.link,
-            tags: tags,
-            tagsRaw: (tags).join(","),
-            author: meta.author,
-            source: meta.source,
-            version: meta.version,
-            lastUpdated: meta.lastUpdated ?? "unknown",
-        };
+		var tags = meta.tags ?? [];
 
-        let html = parseTemplate(templatePage, vars);
+		var content = "";
+		if(fs.existsSync(modsDir + mod + "/desc.md")) {
+			content = fs.readFileSync(modsDir + mod + "/desc.md", 'utf8');
+		}
 
-        var dom = fixHtmlRefs(html, pageDir, _pageDir);
+		var vars = {
+			modName: meta.name,
+			pageTitle: meta.name,
+			cover: imgLink,
+			description: meta.description,
+			link: meta.link,
+			tags: tags,
+			tagsRaw: (tags).join(","),
+			author: meta.author,
+			source: meta.source,
+			version: meta.version,
+			lastUpdated: meta.lastUpdated ?? "unknown",
 
-        console.log(mod);
+			header: header,
+			content: renderer.render(content),
+		};
 
-        //console.log(data);
-        fs.writeFileSync(
-            exportPath + mod + "/index.html",
-            dom.serialize(),
-            'utf8'
-        );
-    }
+		let html = parseTemplate(templatePage, vars);
+
+		var dom = fixHtmlRefs(html, pageDir, _pageDir);
+
+		console.log(mod);
+
+		//console.log(data);
+		fs.writeFileSync(
+			exportPath + mod + "/index.html",
+			dom.serialize(),
+			'utf8'
+		);
+	}
 }
 
 module.exports = {
-    buildMods: buildMods,
-    buildHtml: buildHtml,
+	buildMods: buildMods,
+	buildHtml: buildHtml,
 }
